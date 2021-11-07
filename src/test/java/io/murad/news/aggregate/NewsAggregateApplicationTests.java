a2a837e07acba90876860bdf824efaf663c53d84
Md Murad Hossain <mdmuradhossain33@gmail.com>
2021-11-07 20:30:54 +0600

    project created with required dependencies

---
+++ src/test/java/io/murad/news/aggregate/NewsAggregateApplicationTests.java
@@ -0,0 +1,13 @@
+package io.murad.news.aggregate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsAggregateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
