04e13345d67f9160bb39f34adddf59fdbde71375
Md Murad Hossain <mdmuradhossain33@gmail.com>
2021-11-07 20:47:33 +0600

    WebSecurityConfig and HomeController added

---
+++ src/main/java/io/murad/news/aggregate/config/WebSecurityConfig.java
@@ -0,0 +1,25 @@
+package io.murad.news.aggregate.config;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic();
+        http.authorizeRequests().antMatchers("/").permitAll()
+        .anyRequest().authenticated();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password("admin")
+                .authorities("ADMIN");
+    }
+}
